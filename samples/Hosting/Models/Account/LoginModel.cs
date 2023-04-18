6afaf2ffed682e2f34b6e0bf9afaaae9cbe18966
wangjianfeng <1448376744@qq.com>
2023-04-18 17:16:20 +0800

    移除

--- samples/Hosting/Models/Account/LoginModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Hosting.Models
-{
-    public class LoginModel
-    {
-        public string? ReturnUrl { get; set; }
-        public string? UserName { get; set; }
-        public string? Password { get; set; }
-    }
-}
