543c6c5c72200f7d26492d2a5dddfc58b63a17ad
王剑锋 <1448376744@qq.com>
2023-08-15 09:19:09 +0800

    修改日志

--- src/IdentityServer/Hosting/ApplicationBuilderExtensions.cs
+++ src/IdentityServer/Hosting/ApplicationBuilderExtensions.cs
@@ -20,7 +20,7 @@ namespace Microsoft.AspNetCore.Builder
                 var loggerFactory = app.Services.GetRequiredService<ILoggerFactory>();
                 var clock = app.Services.GetRequiredService<ISystemClock>();
                 var logger = loggerFactory.CreateLogger("IdentityServer.Hosting");
-                logger.LogInformation("  ___    _            _   _ _           ____                           \r\n |_ _|__| | ___ _ __ | |_(_) |_ _   _  / ___|  ___ _ ____   _____ _ __ \r\n  | |/ _` |/ _ \\ '_ \\| __| | __| | | | \\___ \\ / _ \\ '__\\ \\ / / _ \\ '__|\r\n  | | (_| |  __/ | | | |_| | |_| |_| |  ___) |  __/ |   \\ V /  __/ |   \r\n |___\\__,_|\\___|_| |_|\\__|_|\\__|\\__, | |____/ \\___|_|    \\_/ \\___|_|   \r\n                                |___/                                  ");
+                logger.LogInformation("Identityserver started successfully.");
             });
         }
     }
